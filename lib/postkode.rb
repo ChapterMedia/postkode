67cee255f1b318b227cf73f5dcab196622926196
Keith Lawrence <keith@kludge.co.uk>
2015-02-17 15:16:48 +0000

    add postcodes to test, lintproof, update gem info

--- lib/postkode.rb
+++ lib/postkode.rb
@@ -2,29 +2,38 @@
 
 # Handles validation of UK-style postcodes
 class Postkode
-
   # Validating UK Postcodes
   # Correct as at 2014-09-15
   # References below
   # https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/283357/ILRSpecification2013_14Appendix_C_Dec2012_v1.pdf
   # http://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom#Validation
-  # Areas with only single-digit districts: BR, FY, HA, HD, HG, HR, HS, HX, JE, LD, SM, SR, WC, WN, ZE (although WC is always subdivided by a further letter, e.g. WC1A).
+  # Areas with only single-digit districts: BR, FY, HA, HD, HG, HR, HS, HX, JE,
+  #       LD, SM, SR, WC, WN, ZE (although WC is always subdivided by a
+  #       further letter, e.g. WC1A).
   # Areas with only double-digit districts: AB, LL, SO.
-  # Areas with a district '0' (zero): BL, BS, CM, CR, FY, HA, PR, SL, SS (BS is the only area to have both a district 0 and a district 10).
-  # The following central London single-digit districts have been further divided by inserting a letter after the digit and before the space: EC1–EC4 (but not EC50), SW1, W1, WC1, WC2, and part of E1 (E1W), N1 (N1C and N1P), NW1 (NW1W) and SE1 (SE1P).
+  # Areas with a district '0' (zero): BL, BS, CM, CR, FY, HA, PR, SL, SS (BS is
+  #       the only area to have both a district 0 and a district 10).
+  # The following central London single-digit districts have been further
+  #       divided by inserting a letter after the digit and before the space:
+  #       EC1-EC4 (but not EC50), SW1, W1, WC1, WC2, and part of E1 (E1W),
+  #       N1 (N1C and N1P), NW1 (NW1W) and SE1 (SE1P).
   # The letters QVX are not used in the first position.
   # The letters IJZ are not used in the second position.
-  # The only letters to appear in the third position are ABCDEFGHJKPSTUW when the structure starts with A9A.
-  # The only letters to appear in the fourth position are ABEHMNPRVWXY when the structure starts with AA9A.
-  # The final two letters do not use the letters CIKMOV, so as not to resemble digits or each other when hand-written.
-  # Post code sectors are one of ten digits: 0 to 9 with 0 only used once 9 has been used in a post town, save for Croydon and Newport (see above).
+  # The only letters to appear in the third position are ABCDEFGHJKPSTUW when
+  #       the structure starts with A9A.
+  # The only letters to appear in the fourth position are ABEHMNPRVWXY when
+  #       the structure starts with AA9A.
+  # The final two letters do not use the letters CIKMOV, so as not to resemble
+  #       digits or each other when hand-written.
+  # Post code sectors are one of ten digits: 0 to 9 with 0 only used once 9
+  #       has been used in a post town, save for Croydon and Newport (above).
 
-  A1    = "[A-PR-UWYZ]"
-  A2    = "[A-HK-Y]"
-  A3    = "[A-HJKPS-UW]"
-  A4    = "[ABEHMNPRV-XY]"
-  A5    = "[ABD-HJLN-UW-Z]"
-  N     = "[0-9]"
+  A1    = '[A-PR-UWYZ]'
+  A2    = '[A-HK-Y]'
+  A3    = '[A-HJKPS-UW]'
+  A4    = '[ABEHMNPRV-XY]'
+  A5    = '[ABD-HJLN-UW-Z]'
+  N     = '[0-9]'
   AANN  = A1 + A2 + N + N     # the six possible first-part combos
   AANA  = A1 + A2 + N + A4
   ANA   = A1 + N + A3
@@ -39,8 +48,8 @@ class Postkode
   NORMAL_PART_POSTCODE_PATTERN  = /[ ]*(#{PART_ONE})[ ]*/
   NORMAL_POSTCODE_RE = Regexp.new(NORMAL_POSTCODE_VALID, Regexp::IGNORECASE)
 
-  def self.validate(string, return_parts=false)
-    return false if string.nil? || string == "" || string == false
+  def self.validate(string, return_parts = false)
+    return false if string.nil? || string == '' || string == false
     result = string.match(NORMAL_POSTCODE_RE)
     return false if result.nil?
 
@@ -48,14 +57,14 @@ class Postkode
   end
 
   def self.get_first_section(string)
-    self.get_outcode(string)
+    get_outcode(string)
   end
 
   def self.get_outcode(string)
     found = Postkode.validate(string, true)
     found ? found[0] : nil
   end
-  
+
   def self.get_inward(string)
     found = Postkode.validate(string, true)
     found ? found[1] : nil
@@ -63,11 +72,11 @@ class Postkode
 
   def self.find_in_string(string)
     res = string.scan(NORMAL_POSTCODE_PATTERN)
-    res.length>0 ? res : nil
+    res.length > 0 ? res : nil
   end
 
   def self.find_partial_in_string(string)
     res = string.scan(NORMAL_PART_POSTCODE_PATTERN)
-    res.length>0 ? res : nil
+    res.length > 0 ? res : nil
   end
 end
--- spec/postkode.rb
+++ spec/postkode.rb
@@ -5,16 +5,21 @@ describe Postkode do
   describe '.validate' do
     it 'validates valid postcodes' do
       expect(Postkode.validate('WC2E 7PX')).to be true     # AANA
-      expect(Postkode.validate('SW1Y 4BN')).to be true     # AANA # Centralish London
-      expect(Postkode.validate('NE9 6AA')).to be true      # AAN  # Angel of the North
+      expect(Postkode.validate('SW1Y 4BN')).to be true     # AANA
+      expect(Postkode.validate('NE9 6AA')).to be true      # AAN
       expect(Postkode.validate('N1C 4AX')).to be true      # ANA
       expect(Postkode.validate('BS40 5BL')).to be true     # AANN
       expect(Postkode.validate('E17 0DX')).to be true      # ANN
       expect(Postkode.validate('W4 1AP')).to be true       # AN
+
+      # These have caused problems in the past
+      expect(Postkode.validate('WC2N 4AA')).to be true     # AANA
+      expect(Postkode.validate('EC1V 9LA')).to be true     # AANA
     end
 
-    it 'rejects an invalid postcode' do
+    it 'rejects invalid postcodes' do
       expect(Postkode.validate('POSTIE CODE')).to be false
+      expect(Postkode.validate('@ddg5GH')).to be false
     end
 
     it 'rejects a nil postcode' do
@@ -46,13 +51,15 @@ describe Postkode do
 
   describe '.find_in_string' do
     it 'gets a postcode from somewhere in a string' do
-      expect(Postkode.find_in_string('The postcode is in here WC2E 7PX - somewhere')).to eq([['WC2E','7PX']])
+      haystack = 'The postcode is in here WC2E 7PX - somewhere'
+      expect(Postkode.find_in_string(haystack)).to eq([['WC2E', '7PX']])
     end
   end
 
   describe '.find_partial_in_string' do
     it 'gets a partial postcode from somewhere in a string' do
-      expect(Postkode.find_partial_in_string('The postcode is in here WC2E - somewhere')).to eq([['WC2E']])
+      haystack = 'The postcode is in here WC2E - somewhere'
+      expect(Postkode.find_partial_in_string(haystack)).to eq([['WC2E']])
     end
   end
 
